d9db2a975d18531ef980600aca2c9541b1c53e95
sudharaniinti <128570544+sudharaniinti@users.noreply.github.com>
2023-03-29 14:53:11 +0530

    Create Verilog Code.v

---
+++ Verilog Code.v
@@ -0,0 +1,25 @@
+module Clock_Divider(
+    input clk_in,
+    output reg clk_out,
+    input reset,
+    input [31:0] ratio
+    );
+    reg [31:0] counter;
+    always@(posedge clk_in,negedge reset)
+        if(reset==0) begin
+            counter<=32'd0;
+            clk_out<=0;
+        end
+        else
+            if(counter==ratio-1) begin
+                clk_out<=0;
+                counter<=32'd0;
+            end
+            else
+                if(counter==(ratio/2)-1) begin
+                    clk_out<=1;
+                    counter<=counter+1;
+                end
+                else
+                    counter<=counter+1; 
+endmodule
