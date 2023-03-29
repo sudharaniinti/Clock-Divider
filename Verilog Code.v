93dcae90972419cfe0d4cca09a3bb9adcfd94499
sudharaniinti <128570544+sudharaniinti@users.noreply.github.com>
2023-03-29 15:01:28 +0530

    Update Verilog Code.v

--- Verilog Code.v
+++ Verilog Code.v
@@ -1,11 +1,14 @@
+`timescale 1ns / 1ps
 module Clock_Divider(
     input clk_in,
     output reg clk_out,
     input reset,
     input [31:0] ratio
     );
+    
     reg [31:0] counter;
-    always@(posedge clk_in,negedge reset)
+    
+    always@(posedge clk_in,negedge reset) begin
         if(reset==0) begin
             counter<=32'd0;
             clk_out<=0;
@@ -22,4 +25,5 @@ module Clock_Divider(
                 end
                 else
                     counter<=counter+1; 
+    end 
 endmodule
